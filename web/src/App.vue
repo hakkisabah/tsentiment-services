99244b62eba5d62c94016a87f90189a6d1d00d05
hakkisabah <hakkisabah@hotmail.com>
2021-10-27 17:03:37 +0300

    footer information add

--- web/src/App.vue
+++ web/src/App.vue
@@ -20,6 +20,27 @@
           <router-view />
         </v-row>
       </v-container>
+      <v-footer padless>
+        <v-col
+          class='text-center'
+          cols='12'
+        >
+          <p>tsentiment service is open source software and current codes can be reviewed at <a
+            href='https://github.com/hakkisabah/tsentiment-services'>https://github.com/hakkisabah/tsentiment-services</a>.
+          </p>
+          <p>The reason for the necessity of writing the service is the tsentiment package, which is
+            also open sourced at <a href='https://github.com/hakkisabah/tsentiment'>https://github.com/hakkisabah/tsentiment</a>
+            and developed in the R programming language.</p>
+          <p> All code is open source and so your developments will benefit the development of the
+            tsentiment service.</p>
+        </v-col>
+        <v-col
+          class='text-center'
+          cols='12'
+        >
+          {{ new Date().getFullYear() }} — <strong>tsentiment services</strong>
+        </v-col>
+      </v-footer>
     </v-main>
   </v-app>
 </template>
